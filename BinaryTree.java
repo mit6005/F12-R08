5138bd9890fe334b1c080be9c8956da64324fbd5
Katrina Panovich <kp@mit.edu>
2012-10-02 09:33:32 -0400

    adding Binary Tree starter files

---
+++ BinaryTree.java
@@ -0,0 +1,43 @@
+package starting;
+
+import java.util.List;
+
+/**
+ * A binary tree with a left child, right child, and ability to conver to in- or
+ * pre-order list representations.
+ * @param <E> Type of elements stored.
+ */
+public interface BinaryTree<E> {
+    /**
+     * Gets the value at this node.
+     * @return The value at this node.
+     */
+	public E getValue();
+
+    /**
+     * Gets the left subtree.  This is an "optional" method, meaning it may
+     * throw an {@link UnsupportedOperationException}.
+     * @return The right subtree.
+     */
+    public BinaryTree<E> getLeft();
+
+    /**
+     * Gets the right subtree.  This is an "optional" method, meaning it may
+     * throw an {@link UnsupportedOperationException}.
+     * @return The right subtree.
+     */
+    public BinaryTree<E> getRight();
+
+    /**
+     * Creates and returns an in-order list of the elements in this tree (in order
+     * is left child, parent, right child).
+     * @return The in-order list of elements.
+     */
+	public List<E> getInOrder();
+
+	/**
+	 * Creates and returns a pre-order list of the elements in this tree.
+	 * @return The pre-order list of elements.
+	 */
+	public List<E> getPreOrder();
+}
---
+++ BinaryTreeParent.java
@@ -0,0 +1,73 @@
+package starting;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
+/**
+ * A parent tree contains other subtrees as well as a value.
+ * @param <E> The type we store.
+ */
+public class BinaryTreeParent<E> implements BinaryTree<E> {
+    /** The value stored here. */
+	private final E value;
+
+	/** The right child of this parent. */
+	private final BinaryTree<E> right;
+
+	/** The left child of this parent. */
+	private final BinaryTree<E> left;
+
+    /**
+     * Creates a new binary parent tree.
+     * @param value Value at this node.
+     * @param right Right sub-tree.
+     * @param left Left sub-tree.
+     */
+    public BinaryTreeParent(E value, BinaryTree<E> right, BinaryTree<E> left) {
+        this.value = value;
+        this.right = right;
+        this.left = left;
+    }
+
+    @Override
+	/** @see BinaryTree#getValue() */
+    public E getValue() {
+        return value;
+    }
+
+	@Override
+	/** @see BinaryTree#getLeft() */
+	public BinaryTree<E> getLeft() {
+		return left;
+	}
+
+	@Override
+	/** @see BinaryTree#getRight() */
+	public BinaryTree<E> getRight() {
+		return right;
+	}
+
+    @Override
+	/** @see BinaryTree#getInOrder() */
+	public List<E> getInOrder() {
+		List<E> inOrder = left.getInOrder();
+		inOrder.add(value);
+		inOrder.addAll(right.getInOrder());
+
+		return inOrder;
+		
+	}
+
+	@Override
+	/** @see BinaryTree#getPreOrder() */
+	public List<E> getPreOrder() {
+		List<E> preOrder = new ArrayList<E>();
+		preOrder.add(value);
+		preOrder.addAll(left.getPreOrder());
+		preOrder.addAll(right.getPreOrder());
+
+		return preOrder;
+	}
+}
